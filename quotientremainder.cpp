aedccdd69b2b84a24c0722ce71d26a21a341f292
x-darkvanilla-x <122479676+x-darkvanilla-x@users.noreply.github.com>
2023-02-28 23:12:31 +0530

    Add files via upload

---
+++ quotientremainder.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	
+	cout << "Enter Divider and Divisor : ";
+	cin >> a >> b;
+	cout << "Quotient = "<< a/b << "\nRemainder = "<< a%b;
+}
