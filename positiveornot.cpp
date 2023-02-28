94d57f448bf3ab38b35c7eb81c8e4c7233a36c46
x-darkvanilla-x <122479676+x-darkvanilla-x@users.noreply.github.com>
2023-02-28 23:12:18 +0530

    Add files via upload

---
+++ positiveornot.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Enter a number : ";
+	cin >> a;
+	
+	if(a>0)
+	cout << "Positive";
+	else if(a<0)
+	cout << "Negetive";
+	else 
+	cout << "Zero";
+}
